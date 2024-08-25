5fa5f0a26266ad4b47db053a6b82add085530529
Pietro Bernardelli <pietro@wonderflow.ai>
2024-08-25 14:32:04 +0200

    added icon and new colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import Block from "./ui/components/Block";
 import Text from "./ui/components/Text";
 import SocialsBlock from "./ui/components/SocialsBlock";
 import ImageBlock from "./ui/components/ImageBlock";
@@ -20,20 +19,21 @@ export default function Homepage() {
         className="mx-auto flex flex-col md:grid md:max-w-6xl md:grid-flow-dense md:grid-cols-6 gap-4"
       >
         <Text
-          blockClass="bg-[#11634d] text-[#d2c2df] col-span-6"
+          blockClass="bg-[#d2c2df] text-[#11634d] col-span-6"
           heading="Brescia Pride"
           text="Più unite e compatte che mai, ci vogliamo far sentire, per progettare insieme una società a misura di ogni persona."
         />
         <Text
-          blockClass="bg-[#5f370d] text-[#2596be] col-span-6"
+          blockClass="bg-[#2596be] text-[#5f370d] col-span-6"
           text="Ci vediamo in Piazza Vittoria il 7 settembre 2024 alle 15:00!"
           link={{
             href: "https://www.instagram.com/p/C-YFBBoNfoQ/?hl=it&img_index=1",
             text: "Vai al programma completo",
-          }}
+          }
+        }
         />
         <Text
-          blockClass="bg-[#f7bfd1] text-[#a7182a] col-span-3"
+          blockClass="bg-[#d4f3b8] text-[#523376] col-span-3"
           text="Vademecum Accessibilità Corteo Brescia Pride 2024"
           link={{
             href: "https://docs.google.com/document/d/1SkDCVYLZMCDMn6iEdkS6qZnDS-TDDAMhW_sISIoLxSQ/edit?tab=t.0#heading=h.ivu3u5ulwcqw",
@@ -41,7 +41,7 @@ export default function Homepage() {
           }}
         />
         <Text
-          blockClass="bg-[#a7182a] text-[#f7bfd1] col-span-3"
+          blockClass="bg-[#523376] text-[#d4f3b8] col-span-3"
           text="Percorso Corteo Brescia Pride 2024"
           link={{
             href: "https://www.google.com/maps/d/u/0/viewer?mid=1zDgkFCuTgocYO202hmJGve8kp2Jblnw&ll=45.53847900000001%2C10.218770999999997&z=16",
@@ -49,7 +49,7 @@ export default function Homepage() {
           }}
         />
         <Text
-          blockClass="bg-[#f9c224] text-[#033150] col-span-5"
+          blockClass="bg-[#a71729] text-[#f9bfd2] col-span-5"
           text="Alla luce dei recenti avvenimenti, ci uniamo alla voce delle associazioni LGBTQIA+ e T* che da mesi denunciano il costante e preoccupante attacco alla libertà di scelta e autodeterminazione."
           link={{
             href: "https://bresciapride.it/comunicato.pdf",
@@ -57,7 +57,7 @@ export default function Homepage() {
           }}
         />
         <Text
-          blockClass="bg-orange text-cream col-span-6"
+          blockClass="bg-[#5f370d] text-[#2596be] col-span-6"
           text="Siamo una realtà intersezionale, transfemminista, antifascista, gentile e autogestita."
           link={{
             href: "https://bresciapride.it/manifesto.pdf",
@@ -69,10 +69,6 @@ export default function Homepage() {
           altText="Il Corteo"
         />
         <SocialsBlock />
-        <Text
-          blockClass="bg-dark-blue text-light-green col-span-6"
-          text="Lottiamo per l'equità, l'inclusività, la sostenibilità ambientale, e la promozione e il rispetto dei diritti umani."
-        />
          <ImageBlock
           src="/images/16.jpg"
           altText="Disegnando le nostre mascotte"
@@ -82,7 +78,7 @@ export default function Homepage() {
           altText="Panel in un evento"
         />
         <Text
-          blockClass="bg-red text-pink col-span-4"
+          blockClass="bg-[#f9bfd2] text-[#a71729] col-span-4"
           text="Puoi aiutarci con una donazione, anche piccola, per sostenere le nostre attività."
           link={{
             href: "https://www.paypal.com/paypalme/bresciapride",

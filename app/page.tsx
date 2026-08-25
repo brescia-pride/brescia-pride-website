b774e7abc49be4b4f382f782e500ee6e19fc5792
Pietro Bernardelli <pietro@wonderflow.ai>
2026-08-25 10:04:47 +0200

    Blablaqueer

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,7 @@ import localFont from "next/font/local";
 import PrideDate from "./ui/components/PrideDate";
 import Block from "./ui/components/Block";
 import ImageBlock from "./ui/components/ImageBlock";
-import InfoCard from "./ui/components/InfoCard";
-import { Button } from "react-aria-components";
+import ColoredButton from "./ui/components/ColoredButton";
 import UpcomingEventsBlock from "./ui/components/UpcomingEventsBlock";
 const myFont = localFont({ src: "./ui/fonts/ST.ttf" });
 
@@ -43,17 +42,21 @@ export default function Homepage() {
   return (
     <div className="flex-col md:grid md:grid-cols-6">
       <PrideDate showButton={true} />
-      {/* <HomePageBlock className="col-span-6" title="Manifesto" color="purple">
-        <p>
-          abstract manifesto abstract manifesto abstract manifesto abstract
-          manifesto abstract manifesto abstract manifesto abstract manifesto
-          abstract manifesto abstract manifesto abstract manifesto abstract
-          manifesto abstract manifesto abstract manifesto abstract manifesto
-          abstract manifesto abstract manifesto abstract manifesto abstract
-          manifesto abstract manifesto abstract manifesto abstract manifesto
-          abstract manifesto abstract manifesto abstract manifesto
+      <ImageBlock src="/images/Locandina MO.CA blu_upd 24 ago.png" colSpan="col-span-3 p-4" altText="locandina blablaqueer" showOnMobile={true}></ImageBlock>
+      <HomePageBlock className="col-span-3" title="Blablaqueer: Aspettando Brescia Pride" color="blue">
+        <p className="font-bold md:text-3xl">
+          Dall&apos;1 al 3 settembre, riempiamo <Link href="https://morettocavour.com/" className="underline text-blue">MO.CA - Centro per le Nuove Culture</Link> di talk, incontri e momenti di comunità per prepararci al Brescia Pride 2026 con ancora più consapevolezza e prospettive condivise.
         </p>
-      </HomePageBlock> */}
+        <p className="mt-2">
+          Tre giorni per parlare di cosa significa vivere fuori norma e di come le nostre esperienze si intrecciano: dalla cultura ballroom alle esperienze neuroqueer, dall&apos;essere queer e arabə al valore politico dell&apos;educazione sessuale. Ma anche per costruire insieme i cartelloni del Pride, incontrarci, conoscerci e dare spazio alle persone queer che abitano il nostro territorio.
+        </p>
+        <div className="flex flex-row justify-center items-center p-2 gap-4">
+        <ColoredButton text="Clicca qui per partecipare!" bgColor="lightblue" textColor="blue" href="https://forms.gle/Vp7uQqJPnScsSKwe9" className="w-full"></ColoredButton>
+        </div>
+        <p className="mt-2 font-bold">
+          Tutti gli appuntamenti sono gratuiti e accessibili per persone con mobilità ridotta.
+        </p>
+      </HomePageBlock>
       <HomePageBlock
         className="col-span-6"
         title="Prossimi eventi"
